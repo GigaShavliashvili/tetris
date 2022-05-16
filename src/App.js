edf6f6f12ec35e461e5e0419ea512504efeb7a86
GigaShavliashvili <87610428+GigaShavliashvili@users.noreply.github.com>
2022-05-16 15:59:15 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+import "./App.css";
+import Tetris from "./Components/Tetris";
+import { randomTetrominos } from "./Tetrominos";
+const App = () => {
+  return <Tetris />;
+};
 
 export default App;
